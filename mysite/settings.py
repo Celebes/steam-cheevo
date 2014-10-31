d6524c211d7486e4681661173daaf9f128aaeb6e
Celebeze <kris1337@gmail.com>
2014-10-31 16:01:55 +0100

    dodano aplikacje

--- mysite/settings.py
+++ mysite/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+	'cheevo',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -115,3 +116,8 @@ STATICFILES_FINDERS = (
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 DEBUG = False
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
